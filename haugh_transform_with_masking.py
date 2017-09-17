63990b1c1fa48ad386431a291469a8d9fe89dadd
David Pomeroy <pomeroyd@tcd.ie>
2017-09-17 16:09:38 +0100

    commiting all python files and images

---
+++ haugh_transform_with_masking.py
@@ -0,0 +1,78 @@
+# Do relevant imports
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+# Read in and grayscale the image
+image = mpimg.imread('exit-ramp.jpg')
+gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+# Define a kernel size and apply Gaussian smoothing
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+# Define our parameters for Canny and apply
+low_threshold = 50
+high_threshold = 150
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 2
+theta = np.pi/180
+threshold = 15
+min_line_length = 40
+max_line_gap = 20
+line_image = np.copy(image)*0 #creating a blank to draw lines on
+
+# Run Hough on edge detected image
+lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+###############add masking
+
+
+# Pull out the x and y sizes and make a copy of the image
+ysize = lines.shape[0]
+xsize = lines.shape[1]
+region_select = np.copy(lines)
+
+# Define a triangle region of interest 
+# Keep in mind the origin (x=0, y=0) is in the upper left in image processing
+# Note: if you run this code, you'll find these are not sensible values!!
+# But you'll get a chance to play with them soon in a quiz 
+left_bottom = [100, 539]
+right_bottom = [900, 539]
+apex = [450, 300]
+
+# Fit lines (y=Ax+B) to identify the  3 sided region of interest
+# np.polyfit() returns the coefficients [A, B] of the fit
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+# Find the region inside the lines
+XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                    (YY > (XX*fit_right[0] + fit_right[1])) & \
+                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+
+# Color pixels red which are inside the region of interest
+region_select[region_thresholds] = [255, 0, 0]
+
+################
+
+# Iterate over the output "lines" and draw lines on the blank
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+
+# Create a "color" binary image to combine with line image
+color_edges = np.dstack((edges, edges, edges)) 
+
+# Draw the lines on the edge image
+#combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
+combo = cv2.addWeighted(color_edges, 0.8, region_select, 1, 0) 
+plt.imshow(combo)
+plt.show()
